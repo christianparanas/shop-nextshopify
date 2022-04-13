439b46f1ad0fa2503b9eec0134c0edc139e24460
kate <christiannparanas@gmail.com>
2022-04-13 19:08:37 +0800

    Product Page Content

--- pages/products/[product].jsx
+++ pages/products/[product].jsx
@@ -3,7 +3,7 @@ import ProductPageContent from "components/ProductPageContent";
 
 export default function Product({ product }) {
   return (
-    <div className="">
+    <div className="min-h-screen py-12 sm:pt-20">
       <ProductPageContent product={product} />
     </div>
   );
