0784fbfcfb9e09a249b72f2e928eeb11083b4d00
kate <christiannparanas@gmail.com>
2022-04-13 18:26:44 +0800

    feat: get product

---
+++ pages/products/[product].jsx
@@ -0,0 +1,36 @@
+import { getAllProducts, getProduct } from 'lib/shopify'
+
+export default function Product({ product }) {
+  return (
+    <div>
+      {product.title}
+    </div>
+ )
+}
+
+export async function getStaticPaths() {
+  const products = await getAllProducts()
+
+  const paths = products.map(product => {
+    const handle = String(product.node.handle)
+
+    return {
+      params: { product: handle }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const product = await getProduct(params.product)
+
+  return {
+    props: {
+      product
+    }
+  }
+}
